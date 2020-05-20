2315f9c735e2f47c721b748b70f9a08976ae241f
jclementi3 <jclementi3@yahoo.com>
2020-05-19 23:26:24 -0400

    fixxed a bug dealing with iphone apostrope

--- discordbot/Discord_Bot_test.py
+++ discordbot/Discord_Bot_test.py
@@ -5,19 +5,24 @@ from discord.ext.commands import Bot
 
 BOT_PREFIX = ('$')
 
+# opens and reads the .txt file where the bot token is stored
+with open('Discord_Bot_Token.txt', 'r') as reader:
+    token = reader.readline()
 
-#instantiates the discord bot
-client = Bot(command_prefix = BOT_PREFIX)
+# instantiates the discord bot
+
+client = Bot(command_prefix=BOT_PREFIX)
+
 
 @client.command(name='dice')
-async def roll_dice(ctx, dice:str):
+async def roll_dice(ctx, dice: str):
     total_roll = []
     num_dice = int(dice.split('d')[0])
     dice_type = int(dice.split('d')[1])
-    
-    for x in range (0, num_dice):
-        total_roll.append(randint(1,dice_type))
-    rolling  = ' '.join(map(str, total_roll))
+
+    for x in range(0, num_dice):
+        total_roll.append(randint(1, dice_type))
+    rolling = ' '.join(map(str, total_roll))
     rolling += 'Total: ' + str(sum(total_roll))
     await ctx.send(str(rolling))
 
@@ -25,31 +30,31 @@ async def roll_dice(ctx, dice:str):
 @client.command(name='8ball')
 async def eight_ball(ctx):
     possible_responses = [
-    'That is an ASTOUNDING NOPE!',
-    'It is not looking likely',
-    'The future is Hazy',
-    'It does look like a YES',
-    '100 PERCENT YES! YES! YES!']
-    await ctx.send(random.choice(possible_responses) +", "+ ctx.message.author.mention)
+        'That is an ASTOUNDING NOPE!',
+        'It is not looking likely',
+        'The future is Hazy',
+        'It does look like a YES',
+        '100 PERCENT YES! YES! YES!']
+    await ctx.send(random.choice(possible_responses) + ", " + ctx.message.author.mention)
 
 
 @client.command()
-async def square(ctx, number:int):
-    
+async def square(ctx, number: int):
+
     squared_value = int(number)**2
     await ctx.send(str(number) + " squared is " + str(squared_value))
 
 
-
-#once logged in prints confirmation
+# once logged in prints confirmation
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-#when someone types into a channel the bot looks
-#for certain phrases.
-#@client.event
-#async def on_message(message):
+# # # # # Old code Keeping for reference # # # # # # # # #
+# when someone types into a channel the bot looks
+# for certain phrases.
+# @client.event
+# async def on_message(message):
 #    if message.author == client.user:
 #        return
 
@@ -57,22 +62,21 @@ async def on_ready():
 #        await message.channel.send('Hello!')
 #    await client.process_commands(message)
 
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    
-        
     sarcasm = message.content.lower()
     print(sarcasm)
-    sarcasm = str(sarcasm.replace("'", " "))
+    sarcasm = str(sarcasm.replace("'", "").replace("’", ""))
     print(sarcasm[1])
+    print(sarcasm)
 
     if sarcasm.startswith('im '):
         await message.channel.send(f"Hi {sarcasm[2:]}, I'm Test Bot, and I'm the dad joke master")
 
-   
     await client.process_commands(message)
 
-client.run('token goes here')
+client.run(token)
